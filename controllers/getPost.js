34fae8d07c77d945472306ab34905851f92fe5c7
Ahsan Ch <ach374392@gmail.com>
2024-03-03 15:08:02 +0500

    add mri

--- controllers/getPost.js
+++ controllers/getPost.js
@@ -1,6 +1,8 @@
 const  postModel = require("../models/post-model")
 
 const getPost =async (req,res)=>{
+    res.header('Access-Control-Allow-Origin', 'https://pinterst-clone-qox1.vercel.app');
+    res.header('Access-Control-Allow-Credentials', true);
 const Allpost = await postModel.find().populate("user","-posts -password")
 res.status(200).json({message :"All Post" ,Allpost})
 }
