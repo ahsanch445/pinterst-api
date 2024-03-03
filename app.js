ae601a3a7fd4c9e9f8e1250ed43a9c3fa1269656
Ahsan Ch <ach374392@gmail.com>
2024-03-03 14:35:06 +0500

    sai

--- app.js
+++ app.js
@@ -3,21 +3,29 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const cors = require('cors');
-require('dotenv').config();
+const cors = require("cors");
+require("dotenv").config();
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-require('./mongodb/db');
+require("./mongodb/db");
 
 const app = express();
 
-// CORS configuration
+
+
+
+// CORS configuraotion
 const corsOptions = {
   origin: 'https://pinterst-clone-qox1.vercel.app',
   credentials: true,
   optionsSuccessStatus: 204,
 };
 app.use(cors(corsOptions));
+
+// Set up view egine
+app.use(express.static(path.join(__dirname, 'pinterstClone/dist')));
+
+
 
 // Logger and middleware setup
 app.use(logger('dev'));
@@ -32,29 +40,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// Handle preflight requests for '/users/login'
-app.options('/users/login', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://pinterst-clone-qox1.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.status(200).end();
-});
-
 // Catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // Error handler
-app.use(function (err, req, res, next) {
-  // Set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // Render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 // Start the server
 const PORT = process.env.PORT || 3000;
