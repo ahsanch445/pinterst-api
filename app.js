6b29f6584401c19d11daec27f6e8884889618726
Ahsan Ch <ach374392@gmail.com>
2024-03-03 17:44:49 +0500

    my api

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-  origin: 'https://pinterst-clone.vercel.app',
+  origin: 'https://pinterst-mern.vercel.app',
   credentials: true,
   optionsSuccessStatus: 204,
   allowedHeaders: ['Content-Type', 'Authorization'],
